ec660da48f5b35871c15dde4a964ad4ea91cbaa0
WoshuaJolk <joshgwolk@gmail.com>
2023-05-20 12:42:23 -0500

    Settings Page Restyling

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,55 +1,84 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CreditCardIcon from '../assets/icons/credit-card.svg';
-import ProfileIcon from '../assets/icons/user-profile-circle.svg';
-import LibraryIcon from '../assets/icons/library-outline.svg';
-import RightChevronIcon from '../assets/icons/right-chevron.svg';
+import { Color } from "../GlobalStyles";
+import RightChevronIcon from "../assets/icons/right-chevron.svg";
+import UserCircleIcon from "../assets/icons/user-profile-circle.svg";
+import LibraryIcon from "../assets/icons/library-outline.svg";
+import CreditCardIcon from "../assets/icons/credit-card.svg";
 
 import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Settings = ({ navigation }) => {
   const handlePress = (screenName) => {
     navigation.navigate(screenName);
   };
-
+  
   return (
     <SafeAreaView style={styles.pageContainer}>
       <View style={styles.headerContainer}>
         <Text style={styles.headerTitle}>Settings</Text>
-        <View style={styles.divider} />
       </View>
-      <TouchableOpacity
-        style={styles.settingsItemContainer}
-        onPress={() => handlePress("Account")}
-      >
-        <View style={styles.settingsItem}>
-          <ProfileIcon size={24} color="white" style={styles.icon} />
-          <Text style={styles.settingsItemText}>Account</Text>
-        </View>
-        <RightChevronIcon size={24} color="white" style={styles.chevronIcon} />
-      </TouchableOpacity>
-      <View style={styles.divider} />
-      <TouchableOpacity
-        style={styles.settingsItemContainer}
-        onPress={() => handlePress("Stats")}
-      >
-        <View style={styles.settingsItem}>
-          <LibraryIcon size={24} color="white" style={styles.icon} />
-          <Text style={styles.settingsItemText}>Stats</Text>
-        </View>
-        <RightChevronIcon size={24} color="white" style={styles.chevronIcon} />
-      </TouchableOpacity>
-      <View style={styles.divider} />
-      <TouchableOpacity
-        style={styles.settingsItemContainer}
-        onPress={() => handlePress("Billing")}
-      >
-        <View style={styles.settingsItem}>
-          <CreditCardIcon size={24} color="white" style={styles.icon} />
-          <Text style={styles.settingsItemText}>Billing</Text>
-        </View>
-        <RightChevronIcon size={24} color="white" style={styles.chevronIcon} />
-      </TouchableOpacity>
+      <View style={styles.settingsButtonContainer}>
+        <TouchableOpacity 
+						style={styles.settingsButton}
+						onPress={() => handlePress("Account")}
+				>
+          <View style={styles.leftSettingsButton}>
+            <UserCircleIcon
+              width={24}
+              height={24}
+              color={Color.white}
+            />
+            <Text style={styles.buttonTitle}> Account </Text> 
+          </View>
+          <RightChevronIcon
+              width={24}
+              height={24}
+              color={Color.white}
+            />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.settingsButtonContainer}>
+	      <TouchableOpacity 
+						style={styles.settingsButton}
+						onPress={() => handlePress("Stats")}
+				>
+          <View style={styles.leftSettingsButton}>
+            <LibraryIcon
+              width={24}
+              height={24}
+              color={Color.white}
+            />
+            <Text style={styles.buttonTitle}> Stats </Text> 
+          </View>
+          <RightChevronIcon
+              width={24}
+              height={24}
+              color={Color.white}
+            />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.settingsButtonContainer}>
+	      <TouchableOpacity 
+						style={styles.settingsButton}
+						onPress={() => handlePress("Billing")}
+				>
+          <View style={styles.leftSettingsButton}>
+            <CreditCardIcon
+              width={24}
+              height={24}
+              color={Color.white}
+            />
+            <Text style={styles.buttonTitle}> Billing </Text> 
+          </View>
+          <RightChevronIcon
+              width={24}
+              height={24}
+              color={Color.white}
+            />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -61,42 +90,41 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     marginHorizontal: 20,
+    // borderWidth: 1,
+    // borderColor: 'black',
   },
   headerContainer: {
-    marginTop: 10,
+    paddingTop: 20,
+    paddingLeft: 20,
     width: "100%",
-    flexDirection: "column",
+    flexDirection: "col",
+    marginBottom: 20,
+    // borderWidth: 1,
+    // borderColor: 'black',
+  },
+  settingsButtonContainer: {
+    width: "100%",
+    borderTopWidth: 1,
+    borderColor: Color.border_white,
+    marginBottom: 20,
+  },
+  settingsButton: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+  },
+  leftSettingsButton: {
+    flexDirection: "row",
   },
   headerTitle: {
     color: "white",
-    fontSize: 50,
+    fontSize: 30,
     fontWeight: "bold",
   },
-  divider: {
-    borderBottomWidth: 1,
-    borderBottomColor: "white",
-    width: "100%",
-    marginVertical: 10,
-  },
-  settingsItemContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingVertical: 10,
-    width: "100%",
-  },
-  settingsItem: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  icon: {
-    marginRight: 10,
-  },
-  settingsItemText: {
-    fontSize: 20,
+  buttonTitle: {
     color: "white",
-  },
-  chevronIcon: {
-    marginLeft: 10,
+    fontSize: 20,
+    marginLeft: 24,
   },
 });
